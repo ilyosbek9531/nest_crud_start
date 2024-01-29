7fc773e28429a66b1c9ea60493caca2ad7e0a4c5
ilyosbek sheraliyev <ilyosbeksheraliyev@ilyosbeks-MacBook-Air.local>
2024-01-29 22:05:14 +0500

    fixed

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,6 @@ async function bootstrap() {
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
-  await app.listen(3000);
+  await app.listen(8000);
 }
 bootstrap();
